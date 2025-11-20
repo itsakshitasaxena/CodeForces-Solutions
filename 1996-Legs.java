b7b7c6037bd0edd3bab398197f1ff1a781a3ecfe
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-11-20 22:54:54 +0530

    cf practice

---
+++ 1996-Legs.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            if(n%4==0){
+                System.out.println(n/4);
+            }else{
+                System.out.println(n/4+1);
+            }
+        }
+    }
+}
